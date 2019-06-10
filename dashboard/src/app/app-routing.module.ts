2aa747e14456b701fc02aaf6fabadb5da2b7b7d1
Kevin Dahlberg <kevin.dahlberg@cloudburststudio.com>
2019-06-09 22:01:12 -0500

    Added posts view

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { LayoutComponent } from './shared/layout/layout.component';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { PostsComponent } from './views/posts/posts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'posts',
+        component: PostsComponent,
+      }
+    ]
   },
   {
     path: 'login',
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -14,10 +14,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsComponent } from './views/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
